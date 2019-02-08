19e11bd9d7a4ff07847de03a5dbfd5daac47863b
CrossKarol <karolekkawecki@o2.pl>
2019-02-08 21:01:22 +0100

    Próbnu commit z klasami graczy.

---
+++ ShooterGame200/Source/GamePlay/World/Player.cs
@@ -0,0 +1,54 @@
+﻿#region Includes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+#endregion
+
+namespace ShooterGame200
+{
+    public class Player
+    {
+
+        public Hero hero;
+        public List<Unit> units = new List<Unit>();
+        public List<SpawnPoint> spawnpoints = new List<SpawnPoint>();
+
+
+        public Player()
+        {
+
+        }
+
+        public virtual void Update(Player ENEMY, Vector2 OFFSET)
+        {
+
+        }
+
+        public virtual void AddUnit(object INFO)
+        {
+            units.Add((Unit)INFO);
+        }
+
+        public virtual void ChangeScore(int SCORE)
+        {
+
+        }
+
+        public virtual void Draw(Vector2 OFFSET)
+        {
+
+        }
+
+    }
+}
